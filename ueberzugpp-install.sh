c03c02b9e3bdf601c55b47f210dfe5b322cf4f76
alyssa <alyssa>
2023-09-08 00:25:13 +0000

    new

---
+++ ueberzugpp-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+echo "Comprobando si ya tienes ueberzugpp"
+if [ -d "/root/ueberzugpp" ]; then
+	echo "Ya existe. Eliminando"
+	rm -rf "/root/ueberzugpp"
+fi
+
+echo "Vas a instalar ueberzugpp"
+echo " "
+echo "Descargando dependencias"
+sudo xbps-install cmake \
+	libvips-devel \
+	libsixel-devel \
+	chafa-devel \
+	openssl-devel \
+	tbb-devel \
+	xcb-util-image-devel
+
+cd /root
+git clone https://github.com/jstkdng/ueberzugpp
+
+echo "Vamos bien..."
+sleep 0.5s 
+
+cd ueberzugpp
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_OPENCV=OFF ..
+cmake --build .
+
+cp -rv ueberzug /usr/bin/
+
+echo " "
+echo "------------------------"
+echo "Se instalo correctamente"
---
+++ ytfzf-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -d "/root/ytfzf" ]; then
+	echo "se eliminara la carpeta"
+	rm -rv "/root/ytfzf"
+fi
+
+echo " "
+echo "Descargando..."
+
+xbps-install mpv \
+	jq \
+	fzf \
+	curl
+
+cd /root/
+git clone https://github.com/pystardust/ytfzf
+cd ytfzf
+make install doc
+
+echo "------------------------"
+echo "Se instalo correctamente"
+echo "------------------------"
