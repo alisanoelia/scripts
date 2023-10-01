addcbd4495836059c4e610cf2ed252bf97d8e88f
alyssa <alyssa>
2023-10-01 19:31:47 +0000

    add fastfetch install in Void Linux

--- fastfecth-install.sh
+++ fastfecth-install.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+git clone https://github.com/fastfetch-cli/fastfetch
+
+cd fastfetch
+
+mkdir -p build
+cd build
+cmake ..
+cmake --build . --target fastfetch --target flashfetch
+
+cp -rv fastfetch /usr/bin
+
+echo " "
+echo "Instalado correctamente"
