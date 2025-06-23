c560fb253f45ff29e66f7848dc482318b55968ce
asha <asha>
2025-06-23 13:52:20 -0500

    [update]: Update versions scripts

--- brave-install.sh
+++ brave-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-VERSION="1.77.92"
+VERSION="1.78.97"
 
 if [ -d "$HOME/.brave" ]; then
   sudo rm -rfv "$HOME/.brave"
--- fonts.sh
+++ fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v3.3.0"
+VERSION="v3.4.0"
 
 if [ -d "$HOME/.fonts" ]; then
 	echo "La carpeta ~/.fonts ya existe. Eliminándola..."
@@ -19,12 +19,14 @@ sudo xbps-install wget xz
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/${VERSION}/JetBrainsMono.tar.xz
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/${VERSION}/NerdFontsSymbolsOnly.tar.xz
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/${VERSION}/MartianMono.tar.xz
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/${VERSION}/GeistMono.tar.xz
 
 printf "Descomprimiendo las fuentes \n"
 
 tar xvf JetBrainsMono.tar.xz
 tar xvf NerdFontsSymbolsOnly.tar.xz
 tar xvf MartianMono.tar.xz
+tar xvf GeistMono.tar.xz
 
 fc-cache -f -v
 
